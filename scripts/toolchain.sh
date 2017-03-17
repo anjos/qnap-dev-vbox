eeece3dbcb91646728feb65683edc8a15dc168ee
Andre Anjos <andre.dos.anjos@gmail.com>
2017-03-17 17:16:29 +0100

    Fix scripts

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -3,10 +3,10 @@
 URL=https://sourceforge.net/projects/qosgpl/files/QNAP%20NAS%20Tool%20Chains/cross-project-x86.u7-20111011.tar.gz
 FNAME=/tmp/`basename ${URL}`
 
-echo "[>>] Downloading toolchain `${FNAME}' from sourceforge..."
+echo "[>>] Downloading toolchain \`${FNAME}\' from sourceforge..."
 curl -L ${URL} > /${FNAME}
 cd /
-echo "[>>] Extracting toolchain `${FNAME}' from tarball..."
+echo "[>>] Extracting toolchain \`${FNAME}\' from tarball..."
 tar xfz ${FNAME}
-echo "[>>] Removing tarball `${FNAME}'..."
+echo "[>>] Removing tarball \`${FNAME}\'..."
 rm -f ${FNAME}
